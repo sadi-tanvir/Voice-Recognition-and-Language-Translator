d8c70d7fd0b2b69db07215e26b9d193592dc891b
Tanvir Hossain Sadi <htanvir.sadi@gmail.com>
2023-10-10 00:55:37 +0600

    feat - Implemented Language Translation functionality

--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
-console.log("the app is working");
+let langOptions = document.querySelectorAll("select");
+let fromText = document.querySelector(".fromText");
+let translateText = document.querySelector('.toTranslate')
+langOptions.forEach((selectElem, index) => {
+    for (const countryCode in language) {
+        let selected;
+        if (index === 0 && countryCode === 'en-GB') {
+            selected = "selected";
+        } else if (index === 1 && countryCode === 'bn-IN') {
+            selected = "selected";
+        };
+
+        let option = `<option value="${countryCode}" ${selected}>${language[countryCode]}</option>`;
+        selectElem.insertAdjacentHTML('beforeend', option)
+    };
+});
+
+
+fromText?.addEventListener("input", () => {
+    let content = fromText?.value;
+    fromContent = langOptions[0].value;
+    transContent = langOptions[1].value;
+
+    let translateLink = `https://api.mymemory.translated.net/get?q=${content}!&langpair=${fromContent}|${transContent}`;
+
+    fetch(translateLink)
+    .then(translate => translate.json()).then(data => {
+        translateText.value = data.responseData.translatedText;
+    }).catch((error) => {
+        console.error(error);
+    })
+});
